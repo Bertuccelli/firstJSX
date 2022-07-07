9d1fb5147210922c0083fbb4c428aac86a8fe4d7
Amanda Bertuccelli <akbertuccelli@gmail.com>
2022-07-07 11:52:47 -0700

    initial commit?(we'll see if it works!)

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+    <div className="App">
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+            <li>Learn React</li>
+            <li>Wash the Dishes</li>
+            <li>Finish CS homework</li>
+            <li>Feed the Cats (they demand chicken!!) </li>
+        </ul>
+    </div>
+    );
+}
+
+export default App;
+
+// const domContainer = document.querySelector('#root');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(<h1>Hello World!</h1>);
